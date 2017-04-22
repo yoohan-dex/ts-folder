58f77e7f07410943509eb188e51507bf7aa517bd
yoo hoo <yoohoo@yoos-Mac-mini.lan>
2017-04-22 18:57:46 +0800

    lynx!

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,2 @@
-import * as Immutable from 'immutable';
 
-
-export interface DefaultState {
-  [prop: string]: any;
-}
-
-export default class Hello {
-  private hey: string;
-  constructor(defaultState: DefaultState) {
-    this.hey = 'hello';
-  }
-}
+import './lib/lexer';
