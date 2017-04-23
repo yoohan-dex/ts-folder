73add6bae6d78a7d91d61e5ea9f2a3f5d3f9d92e
yoo hoo <yoohoo@yoos-Mac-mini.lan>
2017-04-23 23:02:44 +0800

    - -...

--- lib/src/index.d.ts
+++ lib/src/index.d.ts
@@ -1,1 +1,8 @@
-export * from './lib';
+import { Tree } from './lib/format';
+import { Options } from './lib/typings';
+export declare const parseOption: {
+    voidTags: string[];
+    closingTags: string[];
+    childlessTags: string[];
+};
+export default function parse(str: string, options?: Options): Tree[];
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,38 @@
-export * from './lib';
+import lexer from './lib/lexer';
+import parser from './lib/parser';
+
+import { default as format, Tree } from './lib/format';
+import { Options } from './lib/typings';
+
+
+const childlessTags = [ 'style', 'script', 'template' ];
+
+const closingTags = [
+  'html', 'head', 'body', 'p', 'dt', 'dd', 'li', 'option',
+  'thead', 'th', 'tbody', 'tr', 'td', 'tfoot', 'colgroup',
+];
+
+const voidTags = [
+  '!doctype', 'area', 'base', 'br', 'col', 'command',
+  'embed', 'hr', 'img', 'input', 'keygen', 'link',
+  'meta', 'param', 'source', 'track', 'wbr',
+];
+
+
+export const parseOption = {
+  voidTags,
+  closingTags,
+  childlessTags,
+};
+
+/**
+ * parse the REML and return a ast tree
+ * @param str the original html string
+ * @param options the compiler options
+ */
+export default function parse(str: string, options: Options = parseOption): Tree[] {
+  const tokens = lexer(str, options);
+  const nodes = parser(tokens, options);
+  return format(nodes);
+}
+
